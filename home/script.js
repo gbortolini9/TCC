e6b44cfd6e9293b2812ec6bf3f32fdf13885f943
gbortolini9 <119908312+gbortolini9@users.noreply.github.com>
2025-02-06 09:59:53 -0300

    Criação da Home
    Início da criação da Home

---
+++ home/script.js
@@ -0,0 +1,4 @@
+const startClass = document.querySelector('#iniciar-aula')
+const manageClass = document.querySelector('#gerenciar-turma')
+const manageStudents = document.querySelector('#gerenciar-aluno')
+const manageExercises = document.querySelector('#cadastrar-exercicio')
